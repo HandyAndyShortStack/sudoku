fe10e6e22e16029b6e9be506a41f4581deda159d
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-02 04:05:56 -0800

    do some scaffolding

--- sudoku/src/solve.js
+++ sudoku/src/solve.js
@@ -1,7 +1,11 @@
+var sudoku = require('./sudoku.js');
+
+var input;
+
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', function() {
-  var input = process.stdin.read();
-  if (input !== null) {
-    console.log(input.trim());
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    input = chunk;
   }
 });
