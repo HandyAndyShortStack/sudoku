bc43e577f981e1131404e4ef850c6bc980385877
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-02 21:43:21 -0800

    get open indicies

--- sudoku/spec/sudoku_spec.js
+++ sudoku/spec/sudoku_spec.js
@@ -1,5 +1,25 @@
+var fs = require('fs');
+
 var sudoku = require('../src/sudoku.js');
+var parse = require('../src/parse.js');
 
 describe('sudoku', function() {
 
+  var easyExample = fs.readFileSync('spec/easy_example.csv', 'utf8');
+  var easySudoku;
+
+  beforeEach(function() {
+    easySudoku = sudoku(parse(easyExample));
+  });
+
+  describe('.openIndicies', function() {
+
+    it('has the same length as the number of empty squares', function() {
+      expect(easySudoku.openIndicies.length).toEqual(45);
+    });
+
+    it('is an array of indicies for empty squares', function() {
+      expect(easySudoku.openIndicies.slice(0, 4)).toEqual([0, 1, 2, 5]);
+    });
+  });
 });
--- sudoku/src/sudoku.js
+++ sudoku/src/sudoku.js
@@ -1,8 +1,23 @@
 module.exports = sudoku;
 
-function sudoku(arr) {
+function sudoku(arr, config) {
+
+  if (typeof config === 'undefined') {
+    config = {}
+  }
+
+  if (config.openIndicies) {
+    var openIndicies = openIndicies;
+  } else {
+    var openIndicies = [];
+    arr.forEach(function(item, index) {
+      if (item === 0) {
+        openIndicies.push(index);
+      }
+    });
+  }
 
   return {
-
+    openIndicies: openIndicies
   };
 };
