4abd92a0b0be532e83d9db4e4ebb4ecb102fbc57
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-08 04:42:50 -0800

    switch to iterative solve function

--- sudoku/src/solve.js
+++ sudoku/src/solve.js
@@ -1,5 +1,8 @@
 module.exports = solve;
 
+// The following is the original recursive solve function.
+// Please pardon the CSS style comments; I found the slashes unsightly.
+/*
 function solve(sudoku) {
 
   if (sudoku.isSolved) {
@@ -11,4 +14,29 @@ function solve(sudoku) {
   }
 
   return solve(sudoku.increment());
+}
+*/
+
+// Here is the solve function rewritten iteratively by the babel compiler:
+function solve(_x) {
+  var _again = true;
+
+  _function: while (_again) {
+    var sudoku = _x;
+    _again = false;
+
+    if (sudoku.isSolved) {
+      return sudoku;
+    }
+
+    if (sudoku.isValid) {
+      _x = sudoku.fillNext();
+      _again = true;
+      continue _function;
+    }
+
+    _x = sudoku.increment();
+    _again = true;
+    continue _function;
+  }
 }
